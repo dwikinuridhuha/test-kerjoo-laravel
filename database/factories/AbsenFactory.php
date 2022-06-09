f322317b9797c415b9a94163b320c96f6a2ae881
dwiki <dwiki.nuridhuha@gmail.com>
2022-06-09 14:58:45 +0700

    factory, migrat, and seeder prepare to run

---
+++ database/factories/AbsenFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Absen>
+ */
+class AbsenFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(0, 10),
+            'tanggal' => $this->faker->dateTimeBetween('+1 week', '+3 week'),
+            'alasan' => $this->faker->paragraph(),
+            'active' => $this->faker->boolean(),
+        ];
+    }
+}
